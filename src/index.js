62da485c26afbe4c300905679aa6c3eb85e39054
mincodin9 <mincodin9@gmail.com>
2024-06-25 00:06:19 +0900

    12~15강

--- src/index.js
+++ src/index.js
@@ -12,12 +12,16 @@ import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard'
+import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Blocks />
   </React.StrictMode>
 );
 
